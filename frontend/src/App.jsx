05ffe306250e343b56d0777e920fbd188ff790ac
Kartik <codewithme.kartik@gmail.com>
2025-01-09 00:05:11 +0530

    ui updates and backend finalization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import {Route, Routes} from 'react-router'
+import Home from './components/Home'
+import Layout from './layouts/Layout';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
 function App() {
   return (
-    <>
-      <div>Hello welcome to the app</div>
-    </>
-  )
+    <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/signin" element={<SignIn/>} />
+        <Route exact path="/signup" element={<SignUp/>} />
+    </Routes>
+  );
 }
 
 export default App
